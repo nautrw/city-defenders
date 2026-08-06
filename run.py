5a13d09d3bdd4bd2d1ee279996e1573a07d6fc72
nautrw <nautrw@proton.me>
2026-08-06 00:08:01 -0400

    feat: add basic window

---
+++ run.py
@@ -0,0 +1,5 @@
+from src.app import GameApp
+
+if __name__ == "__main__":
+    app = GameApp()
+    app.run()
---
+++ src/app.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+
+FPS = 60
+
+class GameApp:
+    def __init__(self):
+        pygame.init()
+
+        self.running = True
+
+        self.screen = pygame.display.set_mode((500, 500), pygame.SCALED)
+        self.screen_width = 500
+        self.screen_height = 500
+
+        self.clock = pygame.time.Clock()
+        self.delta_time = 0
+
+    def run(self):
+        try:
+            while self.running:
+                if pygame.event.get(pygame.QUIT):
+                    self.running = False
+
+                pygame.display.flip()
+                self.delta_time = self.clock.tick(FPS) / 1000.0
+        finally:
+            pygame.quit()
+
+if __name__ == "__main__":
+    app = GameApp()
+    app.run()
