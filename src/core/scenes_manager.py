94e736f99a673e468943fe09e8e45174360aef4d
nautrw <nautrw@proton.me>
2026-08-06 00:36:53 -0400

    feat: add scene manager

---
+++ src/core/scenes_manager.py
@@ -0,0 +1,36 @@
+import pygame
+from abc import ABC, abstractmethod
+
+class SceneManager:
+    def __init__(self, screen: pygame.Surface) -> None:
+        self.screen = screen
+        
+        self.current_scene = Scene(self)
+
+    def switch_scene(self, scene: Scene) -> None:
+        self.scene.on_exit()
+        self.scene = scene
+        self.scene.on_enter()
+
+class Scene(ABC):
+    def __init__(self, manager: SceneManager) -> None:
+        self.manager = manager
+
+    # Abstractmethods make it so that its required for any other classes that
+    # inherit from this to implement the functions themselves
+    @abstractmethod
+    def handle_events(self, events: list[pygame.Event]) -> None:
+        ...
+
+    @abstractmethod
+    def update(self, delta_time: int | float) -> None:
+        ...
+
+    def render(self) -> None:
+        pass
+
+    def on_enter(self) -> None:
+        pass
+
+    def on_exit(self) -> None:
+        pass
