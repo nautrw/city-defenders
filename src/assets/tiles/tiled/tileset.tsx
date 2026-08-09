2c4444a023c34d903e8ac6d9c578043c49737cdd
nautrw <nautrw@proton.me>
2026-08-09 15:11:14 -0400

    feat: add tiled tileset
    This will be used to make the maps later on

---
+++ src/assets/tiles/tiled/tileset.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Tileset" tilewidth="26" tileheight="29" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../aseprite/dirt.aseprite" width="26" height="29"/>
+ </tile>
+ <tile id="1">
+  <image source="../aseprite/forest_grass.aseprite" width="26" height="29"/>
+ </tile>
+</tileset>
