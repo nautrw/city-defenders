065bb73a785e5b58e5489a875bcf866b9dff89da
nautrw <nautrw@proton.me>
2026-08-09 21:48:20 -0400

    fix: fix grid being cut off at half

--- src/core/grid.py
+++ src/core/grid.py
@@ -12,15 +12,19 @@ class Grid(pygame.sprite.Sprite):
         self.width = width
         self.height = height
         self._tiles: np.ndarray = np.array([[0 for _ in range(self.width)] for _ in range(self.height)])
-        self._surface = pygame.Surface((self.width * Config.FULL_TILE_WIDTH, self.height * Config.FULL_TILE_HEIGHT))
+        
+        surface_width = (self.width * Config.FULL_TILE_WIDTH) * 2
+        surface_height = (self.height * Config.FULL_TILE_HEIGHT) * 2
+        self._surface = pygame.Surface((surface_width, surface_height))
         self.rect = self._surface.get_rect()
         self.rect.topleft = (screen_x, screen_y)
+        self.grid_drawing_offset = self.width * Config.FULL_TILE_WIDTH
 
     def draw(self, surface: pygame.Surface):
         for y, row in enumerate(self._tiles):
             for x, tile in enumerate(row):
                 tile = TILE_REGISTRY[tile]
-                cartesian_x_position = (x * Config.TILE_TOP_WIDTH) - (y * Config.TILE_TOP_WIDTH)
+                cartesian_x_position = (x * Config.TILE_TOP_WIDTH) - (y * Config.TILE_TOP_WIDTH) + self.grid_drawing_offset
                 cartesian_y_position = (x * Config.TILE_TOP_HEIGHT) + (y * Config.TILE_TOP_HEIGHT)
                 self._surface.blit(tile.image, (cartesian_x_position, cartesian_y_position))
 
