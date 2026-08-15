e69045a62ef66b1b950cd22f22be0106056f63e5
nautrw <nautrw@proton.me>
2026-08-15 19:43:18 -0400

    feat: add new basic tileset

---
+++ src/assets/tiles/tiled/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="../aseprite/tileset.aseprite" width="192" height="96"/>
+</tileset>
