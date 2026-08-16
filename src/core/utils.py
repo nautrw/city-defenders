91e1b4621de5585ab619a881e4f480a364fd5752
nautrw <nautrw@proton.me>
2026-08-15 20:18:01 -0400

    feat: add function to split tilesets into surfaces

---
+++ src/core/utils.py
@@ -0,0 +1,20 @@
+import pygame
+
+def split_tileset(image: pygame.Surface, tile_width: int, tile_height: int) -> dict[int, pygame.Surface]:
+    image_dimensions = image.get_rect().size
+
+    image_tiles_width = image_dimensions[0] // tile_width
+    image_tiles_height = image_dimensions[1] // tile_height
+
+    result = {}
+
+    for tile_x in range(image_tiles_width):
+        for tile_y in range(image_tiles_height):
+            tile_i = tile_y * image_tiles_width + tile_x
+
+            tile_rect_left = (tile_x * tile_width) % image_dimensions[0]
+            tile_rect_top = (tile_y * tile_height) % image_dimensions[1]
+
+            result[tile_i] = image.subsurface(tile_rect_left, tile_rect_top, tile_width, tile_height)
+
+    return result
