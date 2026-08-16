587baae491f0f0dcbe8e4651bc9466bdcccddc7f
nautrw <nautrw@proton.me>
2026-08-15 22:40:53 -0400

    refactor: move map into the main game scene

--- src/scenes/main_game.py
+++ src/scenes/main_game.py
@@ -1,7 +1,6 @@
 import pygame
 from src.core.scenes_manager import Scene, SceneManager
 from src.core.config import config as Config
-from src.core.map import Map
 from src.core.utils import split_tileset
 from typing import TYPE_CHECKING
 
@@ -13,29 +12,21 @@ if TYPE_CHECKING:
 
 
 class MainGameScene(Scene):
-    def __init__(self, game: GameApp):
+    def __init__(self, game: GameApp, map_data: list[list[int]]):
         super().__init__(game)
-        data = [
-         [  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 11,  8,  8, ],
-         [  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 11,  8,  8, ],
-         [  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 11,  8,  8, ],
-         [  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 11,  8,  8, ],
-         [  8,  8,  8, 11, 11, 11, 11, 11, 11,  8,  8,  8, 11,  8,  8, ],
-         [  8,  8,  8, 11,  8,  8,  8,  8, 11,  8,  8,  8, 11,  8,  8, ],
-         [  8,  8,  8, 11,  8,  8,  8,  8, 11,  8,  8,  8, 11,  8,  8, ],
-         [  8,  8,  8, 11,  8,  8,  8,  8, 11,  8,  8,  8, 11,  8,  8, ],
-         [  8,  8,  8, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,  8,  8, ],
-         [  8,  8,  8,  8,  8,  8,  8,  8, 11,  8,  8,  8,  8,  8,  8, ],
-         [  8,  8,  8, 11, 11, 11,  8,  8, 11,  8,  8,  8,  8,  8,  8, ],
-         [  8,  8,  8, 11,  8, 11,  8,  8, 11,  8,  8,  8,  8,  8,  8, ],
-         [  8,  8,  8, 11, 11, 11, 11, 11, 11,  8,  8,  8,  8,  8,  8, ],
-         [  8,  8,  8,  8,  8, 11,  8,  8,  8,  8,  8,  8,  8,  8,  8, ],
-         [  8,  8,  8,  8,  8, 11,  8,  8,  8,  8,  8,  8,  8,  8,  8, ],
-        ]
 
-        tileset = pygame.image.load("src/assets/tiles/tileset.png").convert()
-        tiles = split_tileset(tileset, Config.TILE_WIDTH, Config.TILE_HEIGHT)
-        self.map = Map(tiles, data)
+        self.tileset_img = pygame.image.load("src/assets/tiles/tileset.png").convert()
+        self.tileset = split_tileset(self.tileset_img, Config.TILE_WIDTH, Config.TILE_HEIGHT)
+
+        self.tiles_width = len(map_data[0])
+        self.tiles_height = len(map_data)
+
+        self.map_width = self.tiles_width * Config.TILE_WIDTH
+        self.map_height = self.tiles_height * Config.TILE_HEIGHT
+
+        self.map_data = map_data
+        self.map_surf = pygame.Surface((self.map_width, self.map_height))
+        self.map_rect = self.map_surf.get_rect(topleft=(0,0))
 
     def handle_events(self, events: list[pygame.Event]) -> None:
         for event in events:
@@ -43,7 +34,13 @@ class MainGameScene(Scene):
 
     def update(self, delta_time: int | float) -> None:
         pass
+    
+    def _draw_map(self) -> None:
+        for y, row in enumerate(self.map_data):
+            for x, tile_id in enumerate(row):
+                self.map_surf.blit(self.tileset[tile_id], (x * Config.TILE_WIDTH, y * Config.TILE_HEIGHT))
 
     def render(self, surface: pygame.Surface) -> None:
         surface.fill("black")
-        self.map.draw(self.game.screen)
+        self._draw_map()
+        surface.blit(self.map_surf, self.map_rect)
