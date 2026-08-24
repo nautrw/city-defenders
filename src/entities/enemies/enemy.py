03509d2c66fbc8a2489c798ebababc653b0ad128
nautrw <nautrw@proton.me>
2026-08-24 16:20:09 -0400

    feat: add enemy movement

--- src/entities/enemies/enemy.py
+++ src/entities/enemies/enemy.py
@@ -5,11 +5,17 @@ class Enemy(pygame.sprite.Sprite):
     image: pygame.Surface
     rect: pygame.Rect | pygame.FRect
 
-    def __init__(self, image: pygame.Surface, movement_speed: int, max_health: int):
+    def __init__(self, image: pygame.Surface, movement_speed: int, max_health: int, path_waypoints: list[tuple[float, float]]):
         super().__init__()
 
         self.image = image
         self.rect = self.image.get_rect()
+
+        self.path_waypoints = path_waypoints
+        self.waypoint_index = 1
+        self.movement_target = self.path_waypoints[self.waypoint_index]
+        self.position = pygame.Vector2(self.path_waypoints[0])
+        self.velocity = pygame.Vector2()
 
         self.movement_speed = movement_speed
         self.max_health = max_health
@@ -17,3 +23,18 @@ class Enemy(pygame.sprite.Sprite):
 
     def draw(self, surface: pygame.Surface):
         surface.blit(self.image, self.rect)
+    
+    def update(self, dt: float) -> None:
+        if self.waypoint_index < len(self.path_waypoints):
+            heading = self.movement_target - pygame.Vector2(self.rect.center)
+            distance_to_target = heading.length()
+            heading.normalize_ip()
+
+            if distance_to_target <= 2:
+                self.waypoint_index += 1
+                self.movement_target = self.path_waypoints[self.waypoint_index]
+
+            self.velocity = heading * self.movement_speed * dt
+
+        self.position += self.velocity
+        self.rect.center = self.position
