6608c450ec7bd915f785c7429ad60fb3e161e25e
nautrw <nautrw@proton.me>
2026-08-24 16:20:27 -0400

    refactor: make slime have stats

--- src/entities/enemies/slime.py
+++ src/entities/enemies/slime.py
@@ -9,11 +9,9 @@ class Slime(Enemy):
     image: pygame.Surface
     rect: pygame.Rect | pygame.FRect
 
-    def __init__(self, x: float, y: float):
+    def __init__(self, path_waypoints: list[tuple[float, float]]):
         image = pygame.image.load(
             path.join("src", "assets", "entities", "slime.png")
         ).convert_alpha()
 
-        super().__init__(image=image, movement_speed=5, max_health=10)
-
-        self.rect.center = (x, y)
+        super().__init__(image=image, movement_speed=50, max_health=10, path_waypoints=path_waypoints)
