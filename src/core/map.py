f786dd870e8e2e31399f7dfe21418d24d1a5dda6
nautrw <nautrw@proton.me>
2026-08-23 20:10:14 -0400

    fix: fix weird rendering in map

--- src/core/map.py
+++ src/core/map.py
@@ -25,28 +25,37 @@ class GameMap(pygame.sprite.Sprite):
 
         self.tiles_width = map_data["width"]
         self.tiles_height = map_data["height"]
+
         self.map_width = self.tiles_width * Config.TILE_WIDTH
         self.map_height = self.tiles_height * Config.TILE_HEIGHT
         
         self.ground_tiles = pygame.sprite.Group()
         self.path_tiles = pygame.sprite.Group()
 
+        self._load_layer(Config.GROUND_TILES_LAYER_NAME, self.ground_tiles)
+        self._load_layer(Config.PATH_TILES_LAYER_NAME, self.path_tiles)
+
         self.image = pygame.Surface((self.map_width, self.map_height))
-
-        for y, row in enumerate(self.map_data["layers"][Config.GROUND_TILES_LAYER_NAME]["data"]):
-            for x, tile_id in enumerate(row):
-                new_tile = MapTile(x, y, self.tileset[tile_id])
-                new_tile.add(self.ground_tiles)
-                self.image.blit(new_tile.image, new_tile.rect)
-
-        for y, row in enumerate(self.map_data["layers"][Config.PATH_TILES_LAYER_NAME]["data"]):
-            for x, tile_id in enumerate(row):
-                if not tile_id == -1:
-                    new_tile = MapTile(x, y, self.tileset[tile_id])
-                    new_tile.add(self.path_tiles)
-                    self.image.blit(new_tile.image, new_tile.rect)
+        self._redraw()
 
         self.rect = self.image.get_rect(topleft=(0, 0))
+
+    def _load_layer(self, layer_name: str, group: pygame.sprite.Group) -> None:
+        layer = self.map_data["layers"][layer_name]["data"]
+
+        for y, row in enumerate(layer):
+            for x, tile_id in enumerate(row):
+                if tile_id == -1: # empty
+                    continue
+                
+                tile = MapTile(x, y, self.tileset[tile_id])
+                group.add(tile)
+
+    def _redraw(self) -> None:
+        self.image.fill((0, 0, 0, 0))
+
+        self.ground_tiles.draw(self.image)
+        self.path_tiles.draw(self.image)
 
     def update(self, dt: int | float) -> None:
         pass
