55b3b5db10f47b2d211fd621757ecc98e34703b6
nautrw <nautrw@proton.me>
2026-08-24 23:42:56 -0400

    feat: rotate arrow when shot

--- src/entities/projectiles/ballistic_projectile.py
+++ src/entities/projectiles/ballistic_projectile.py
@@ -1,6 +1,6 @@
 import pygame
 
-from src.core.utils import Coordinate
+from src.core.utils import Coordinate, angle_to_point
 
 
 class BallisticProjectile(pygame.sprite.Sprite):
@@ -10,13 +10,16 @@ class BallisticProjectile(pygame.sprite.Sprite):
     def __init__(self, position: Coordinate, target: Coordinate, image: pygame.Surface, movement_speed: int):
         super().__init__()
 
-        self.image = image
+        self.original_image = image
+        self.image = self.original_image.copy()
         self.rect = image.get_rect(centerx=position[0], bottom=position[1])
 
         self.position = pygame.Vector2(position)
         self.velocity = pygame.Vector2()
         self.movement_speed = movement_speed
         self.target = target
+        
+        self.angle = 0
 
     def update(self, dt: float) -> None:
         movement = self.target - pygame.Vector2(self.rect.center)
@@ -24,6 +27,9 @@ class BallisticProjectile(pygame.sprite.Sprite):
         self.velocity = movement * self.movement_speed
         self.position += self.velocity * dt
         self.rect.center = self.position
+        
+        self.angle = angle_to_point(self.position, self.target)
 
     def draw(self, surface: pygame.Surface):
+        self.image = pygame.transform.rotate(self.original_image, -self.angle)
         surface.blit(self.image, self.rect)
