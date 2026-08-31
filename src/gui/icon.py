e209d7f0d429c0dd7062b3e1d579d205d62a6112
nautrw <nautrw@proton.me>
2026-08-31 17:59:21 -0400

    feat: add icon element

---
+++ src/gui/icon.py
@@ -0,0 +1,18 @@
+import pygame
+
+from src.gui.element import Element
+
+
+class Icon(Element):
+    def __init__(self, id: str, x: int, y: int, image: pygame.Surface):
+        self.id = id
+        self.image = image
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+        super().__init__(self.id, self.image, self.rect)
+
+    def draw(self, surface: pygame.Surface) -> None:
+        surface.blit(self.image, self.rect)
+
+    def update(self, delta_time: float, mouse_position: tuple[int, int]) -> None:
+        pass
