c2ec0ac5b7c648949dd036b93941e81575501c19
nautrw <nautrw@proton.me>
2026-09-02 17:28:53 -0400

    feat: add placement anchor system for elements

---
+++ src/gui/placement_system.py
@@ -0,0 +1,13 @@
+from enum import Enum, auto
+
+
+class RectAnchorMode(Enum):
+    TOPLEFT = auto()
+    MIDTOP = auto()
+    TOPRIGHT = auto()
+    MIDLEFT = auto()
+    CENTER = auto()
+    MIDRIGHT = auto()
+    BOTTOMLEFT = auto()
+    MIDBOTTOM = auto()
+    BOTTOMRIGHT = auto()
