657a960f62de61f68658d48e2d54426366c9c49c
nautrw <nautrw@proton.me>
2026-09-03 21:08:55 -0400

    fix: fix discard button positioning

--- src/scenes/main_game.py
+++ src/scenes/main_game.py
@@ -240,7 +240,6 @@ class MainGameSceneGUIManager(GUIManager):
                 "tower_discard_button",
                 (
                     Config.SCREEN_WIDTH
-                    - Config.BUTTON_SIZE
                     - Config.ELEMENT_OUTER_PADDING
                 ),
                 Config.ELEMENT_OUTER_PADDING,
