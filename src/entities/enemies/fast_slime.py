854d8a659df1078dd5b4589edce02c512fc84975
nautrw <nautrw@proton.me>
2026-09-09 18:52:50 -0400

    feat: add fast slime

---
+++ src/entities/enemies/fast_slime.py
@@ -0,0 +1,16 @@
+import pygame
+
+from src.core.utils import load_asset
+from src.entities.enemies.enemy import Enemy
+
+
+class FastSlime(Enemy):
+    image: pygame.Surface
+    rect: pygame.Rect | pygame.FRect
+
+    def __init__(self, path_waypoints: list[tuple[float, float]]):
+        animation = [load_asset("fastslime1"), load_asset("fastslime2")]
+
+        super().__init__(
+            animation=animation, movement_speed=100, max_health=5, path_waypoints=path_waypoints, coins_drop=30
+        )
