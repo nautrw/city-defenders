0c3efb1df479b7013868298f62613277182896c9
nautrw <nautrw@proton.me>
2026-09-09 17:19:49 -0400

    refactor: fill the background with green when the game is won

--- src/scenes/game_won.py
+++ src/scenes/game_won.py
@@ -21,6 +21,7 @@ class GameWonScene(Scene):
         pass
 
     def render(self, surface: pygame.Surface) -> None:
+        surface.fill(Config.FULL_HEALTH_COLOR)
         font = pygame.font.Font(get_font(Config.FONT_NORMAL), Config.FONT_SIZE_VERYBIG)
         text = font.render("Game Over", True, Config.TEXT_COLOR_NORMAL)
         text_rect = text.get_frect(
