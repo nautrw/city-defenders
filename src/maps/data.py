350bcb7f21bc308864e2161e3f721837a72359c6
nautrw <nautrw@proton.me>
2026-09-09 09:25:34 -0400

    refactor: add waves to the main test map

--- src/maps/data.py
+++ src/maps/data.py
@@ -3,7 +3,9 @@ MAPS_DATA = {
         "waves": {
             0: ("slime",),
             1: ("slime", "slime"),
-            2: ("slime", "slime", "slime")
+            2: ("slime", "slime", "slime"),
+            3: ("slime", "slime", "slime", "slime"),
+            4: ("slime", "slime", "slime", "slime", "slime")
         },
         "waves_interval": 15,
         "initial_balance": 190,
