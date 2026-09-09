ca8faee65383be881fd922c0a6bc24a78de62b03
nautrw <nautrw@proton.me>
2026-09-09 18:55:25 -0400

    refactor: add fast slime to entity data dict

--- src/entities/entity_data.py
+++ src/entities/entity_data.py
@@ -1,5 +1,6 @@
+from src.entities.enemies.fast_slime import FastSlime
 from src.entities.enemies.slime import Slime
 from src.entities.turrets.crossbow import CrossbowTurret
 
-ENEMIES = {"slime": Slime}
+ENEMIES = {"slime": Slime, "fast_slime": FastSlime}
 TURRETS = {"crossbow": CrossbowTurret}
