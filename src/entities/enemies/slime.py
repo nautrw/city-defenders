521233e8238624f416fd770d0065a1b53432f1d3
nautrw <nautrw@proton.me>
2026-09-09 18:52:56 -0400

    refactor: make slimes drop more coins

--- src/entities/enemies/slime.py
+++ src/entities/enemies/slime.py
@@ -12,5 +12,5 @@ class Slime(Enemy):
         animation = [load_asset("slime1"), load_asset("slime2")]
 
         super().__init__(
-            animation=animation, movement_speed=50, max_health=10, path_waypoints=path_waypoints, coins_drop=10
+            animation=animation, movement_speed=50, max_health=10, path_waypoints=path_waypoints, coins_drop=25
         )
