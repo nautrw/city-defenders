c2f9c66d8bacb61e14d33222bca8816c4c0e610c
nautrw <nautrw@proton.me>
2026-09-09 20:21:26 -0400

    feat: add ballista turret

--- src/entities/entity_data.py
+++ src/entities/entity_data.py
@@ -1,6 +1,7 @@
 from src.entities.enemies.fast_slime import FastSlime
 from src.entities.enemies.slime import Slime
+from src.entities.turrets.ballista import BallistaTurret
 from src.entities.turrets.crossbow import CrossbowTurret
 
 ENEMIES = {"slime": Slime, "fast_slime": FastSlime}
-TURRETS = {"crossbow": CrossbowTurret}
+TURRETS = {"crossbow": CrossbowTurret, "ballista": BallistaTurret}
---
+++ src/entities/turrets/ballista.py
@@ -0,0 +1,29 @@
+import pygame
+
+from src.core.utils import load_asset
+from src.entities.projectiles.arrow import Arrow
+from src.entities.turrets.turret import Turret
+
+
+class BallistaTurret(Turret):
+    image: pygame.Surface
+    rect: pygame.Rect | pygame.FRect
+
+    display_name = "Ballista"
+    description = "Shoots bolts at a slow rate but with great force."
+    cost = 250
+
+    def __init__(self, x_position: int, y_position: int):
+        image = load_asset("ballista")
+
+        super().__init__(
+            display_name=self.display_name,
+            description=self.description,
+            cost=self.cost,
+            x_position=x_position,
+            y_position=y_position,
+            turret_image=image,
+            projectile=Arrow,
+            shooting_speed=3,
+            area_radius=75,
+        )
