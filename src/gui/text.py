9f932e987f2894767f3b45d5810e752c8ca6b6d9
nautrw <nautrw@proton.me>
2026-09-10 09:55:37 -0400

    refactor: default text element wrap length to 0

--- src/gui/text.py
+++ src/gui/text.py
@@ -24,7 +24,7 @@ class Text(Element):
         antialias: bool = True,
         fg_color: ColorLike = Config.TEXT_COLOR_NORMAL,
         font_name: str = Config.FONT_NORMAL,
-        wrap_length: int = 500,
+        wrap_length: int = 0,
     ):
         self.font = pygame.font.Font(get_font(font_name), size)
         self.image = pygame.Surface((0, 0))  # placeholder
