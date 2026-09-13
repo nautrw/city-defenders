3390a0bef842665ac2760fbcf7236cf73af06f80
nautrw <nautrw@proton.me>
2026-09-13 15:07:15 -0400

    feat: add next wave button

--- src/scenes/main_game_gui_manager.py
+++ src/scenes/main_game_gui_manager.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 import pygame
 
 import src.core.config as Config
-from src.core.utils import load_asset, load_scaled_asset
+from src.core.utils import load_asset, load_scaled_asset, load_button_state_triplet_assets
 from src.entities.entity_data import TURRETS
 from src.entities.turrets.turret import Turret
 from src.gui.button import CUSTOM_BUTTON_CLICKED, Button
@@ -168,7 +168,19 @@ class MainGameSceneGUIManager(GUIManager):
                 pressed_icon=build_icon,
                 anchor=RectAnchorMode.TOPRIGHT,
             )
+
+            next_wave_button = Button(
+                "next_wave_button",
+                (Config.SCREEN_WIDTH - Config.ELEMENT_OUTER_PADDING),
+                (Config.SCREEN_HEIGHT - Config.ELEMENT_OUTER_PADDING),
+                Config.BUTTON_SIZE,
+                Config.BUTTON_SIZE,
+                **load_button_state_triplet_assets("next_wave"),
+                anchor=RectAnchorMode.BOTTOMRIGHT
+            )
+
             self.elements.append(build_button)
+            self.elements.append(next_wave_button)
         elif self.state == UIStates.TOWER_PICKER_MENU:
             container_width = 500
 
