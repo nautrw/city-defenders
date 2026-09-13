15e1d7de5b852eee307b68c5dbe55855c012bf8e
nautrw <nautrw@proton.me>
2026-09-13 16:23:48 -0400

    fix: fix press start text showing after wave 0

--- src/scenes/main_game_gui_manager.py
+++ src/scenes/main_game_gui_manager.py
@@ -137,7 +137,7 @@ class MainGameSceneGUIManager(GUIManager):
 
         wave_text = Text(
             "wave_text",
-            f"Wave {self.scene.wave + 1}/{len(self.scene.waves)}" if self.scene.wave > 0 else "Press Start",  # ty:ignore[unresolved-attribute]
+            f"Wave {self.scene.wave + 1}/{len(self.scene.waves)}" if self.scene.wave != -1 else "Press Start",  # ty:ignore[unresolved-attribute]
             Config.ELEMENT_OUTER_PADDING,
             wave_display_container.rect.height / 2,
             anchor=RectAnchorMode.MIDLEFT
