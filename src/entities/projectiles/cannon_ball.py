09ce95562d8c991f9ca2041e660b46ccbff9f589
nautrw <nautrw@proton.me>
2026-09-14 16:21:55 -0400

    feat: add cannon ball projectile

---
+++ src/entities/projectiles/cannon_ball.py
@@ -0,0 +1,23 @@
+import pygame
+
+from src.core.utils import load_asset
+from src.entities.enemies.enemy import Enemy
+from src.entities.projectiles.ballistic_projectile import BallisticProjectile
+
+
+class CannonBall(BallisticProjectile):
+    image: pygame.Surface
+    rect: pygame.Rect | pygame.FRect
+
+    def __init__(
+        self, damage: int, x_position: float, y_position: float, target: Enemy
+    ):
+        image = load_asset("cannon_ball")
+
+        super().__init__(
+            x_position=x_position,
+            y_position=y_position,
+            target=target,
+            image=image,
+            damage=damage,
+        )
