bf3144a7798d64f1ae65873685499ba5f5a6376c
nautrw <nautrw@proton.me>
2026-09-14 16:21:59 -0400

    feat: add cannon turret

--- src/entities/entity_data.py
+++ src/entities/entity_data.py
@@ -1,7 +1,12 @@
 from src.entities.enemies.fast_slime import FastSlime
 from src.entities.enemies.slime import Slime
 from src.entities.turrets.ballista import BallistaTurret
+from src.entities.turrets.cannon import CannonTurret
 from src.entities.turrets.crossbow import CrossbowTurret
 
 ENEMIES = {"slime": Slime, "fast_slime": FastSlime}
-TURRETS = {"crossbow": CrossbowTurret, "ballista": BallistaTurret}
+TURRETS = {
+    "crossbow": CrossbowTurret,
+    "ballista": BallistaTurret,
+    "cannon": CannonTurret,
+}
---
+++ src/entities/turrets/cannon.py
@@ -0,0 +1,32 @@
+import pygame
+
+from src.core.utils import load_asset
+from src.entities.projectiles.cannon_ball import CannonBall
+from src.entities.turrets.turret import Turret
+
+
+class CannonTurret(Turret):
+    image: pygame.Surface
+    rect: pygame.Rect | pygame.FRect
+
+    display_name = "Cannon"
+    description = (
+        "Shoots cannon balls at turrets. Does massive amounts of damage. Very slow."
+    )
+    cost = 100
+
+    def __init__(self, x_position: int, y_position: int):
+        image = load_asset("cannon")
+
+        super().__init__(
+            display_name=self.display_name,
+            description=self.description,
+            cost=self.cost,
+            damage=25,
+            x_position=x_position,
+            y_position=y_position,
+            turret_image=image,
+            projectile=CannonBall,
+            shooting_speed=3,
+            area_radius=75,
+        )
