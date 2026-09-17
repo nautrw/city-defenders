f1ccc12543afdab81dd4da7d5f2480096054e328
nautrw <nautrw@proton.me>
2026-09-17 19:26:40 -0400

    feat: add tank slime entity

---
+++ src/entities/enemies/tank_slime.py
@@ -0,0 +1,21 @@
+import pygame
+
+from src.core.utils import load_asset
+from src.entities.enemies.enemy import Enemy
+
+
+class TankSlime(Enemy):
+    image: pygame.Surface
+    rect: pygame.Rect | pygame.FRect
+
+    def __init__(self, path_waypoints: list[tuple[float, float]]):
+        animation = [load_asset("tankslime1"), load_asset("tankslime2")]
+
+        super().__init__(
+            animation=animation,
+            movement_speed=10,
+            animation_duration=0.25,
+            max_health=100,
+            path_waypoints=path_waypoints,
+            coins_drop=250,
+        )
--- src/entities/entity_data.py
+++ src/entities/entity_data.py
@@ -1,10 +1,11 @@
 from src.entities.enemies.fast_slime import FastSlime
 from src.entities.enemies.slime import Slime
+from src.entities.enemies.tank_slime import TankSlime
 from src.entities.turrets.ballista import BallistaTurret
 from src.entities.turrets.cannon import CannonTurret
 from src.entities.turrets.crossbow import CrossbowTurret
 
-ENEMIES = {"slime": Slime, "fast_slime": FastSlime}
+ENEMIES = {"slime": Slime, "fast_slime": FastSlime, "tank_slime": TankSlime}
 TURRETS = {
     "crossbow": CrossbowTurret,
     "ballista": BallistaTurret,
