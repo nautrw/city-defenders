8571bc2119d8714e62d2f7d36eca26ca907223c8
nautrw <nautrw@proton.me>
2026-09-18 20:06:10 -0400

    refactor: add frostspire to entity data

--- src/entities/entity_data.py
+++ src/entities/entity_data.py
@@ -4,10 +4,12 @@ from src.entities.enemies.tank_slime import TankSlime
 from src.entities.turrets.ballista import BallistaTurret
 from src.entities.turrets.cannon import CannonTurret
 from src.entities.turrets.crossbow import CrossbowTurret
+from src.entities.turrets.frostspire import Frostspire
 
 ENEMIES = {"slime": Slime, "fast_slime": FastSlime, "tank_slime": TankSlime}
 TURRETS = {
     "crossbow": CrossbowTurret,
     "ballista": BallistaTurret,
     "cannon": CannonTurret,
+    "frostspire": Frostspire,
 }
