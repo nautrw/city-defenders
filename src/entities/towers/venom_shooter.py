145ce0a0ea18d11bd85801924e1e714471228c35
nautrw <nautrw@proton.me>
2026-09-24 09:03:56 -0400

    refactor: make venom shooter do damage

--- src/entities/towers/venom_shooter.py
+++ src/entities/towers/venom_shooter.py
@@ -26,7 +26,7 @@ class VenomShooterTower(Tower):
             display_name=self.display_name,
             description=self.description,
             cost=[self.initial_cost, 300, 450],
-            damage=[0, 0, 0],
+            damage=[0.1, 0.2, 0.3],
             x_position=x_position,
             y_position=y_position,
             tower_image=image,
